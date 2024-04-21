cf60426d21e4c9de591f1ea51868dbe45420c3f9
Aaryan6 <aaryanpatel683@gmail.com>
2024-04-21 14:45:18 +0530

    change readme

--- src/app/api/send-email/webhook/route.ts
+++ src/app/api/send-email/webhook/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request, res: Response) {
       email: data.record.email,
       phone: data.record.phone,
     }),
-    text: "New Course Enrollment Email", // Add the 'text' property with a value
+    text: "New Course Enrollment Email",
   });
 
   return NextResponse.json(
@@ -38,26 +38,3 @@ export async function POST(req: Request, res: Response) {
     { status: 200 }
   );
 }
-
-// {
-//   "message": "Course enrollment email sent successfully",
-//   "data": {
-//       "type": "INSERT",
-//       "table": "course-customers",
-//       "record": {
-//           "id": "9605bd45-f674-4b0d-94fa-1cee2773b546",
-//           "name": "test",
-//           "email": "test@test.t",
-//           "phone": "34235",
-//           "price": null,
-//           "created_at": "2024-02-28T09:59:49.804321+00:00",
-//           "updated_at": null,
-//           "isPurchased": null,
-//           "hostedInvoiceUrl": null,
-//           "stripeCheckoutId": null,
-//           "stripeCustomerId": null
-//       },
-//       "schema": "public",
-//       "old_record": null
-//   }
-// }
